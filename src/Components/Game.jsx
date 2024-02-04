54a0876da42fe745a1b99418d48873611e5f3c28
Ashwin Sunuwar <ashwinsunuwarr2@gmail.com>
2024-02-04 12:36:38 +0545

    time-travel

--- src/Components/Game.jsx
+++ src/Components/Game.jsx
@@ -4,19 +4,44 @@ import Board from "./Board";
 function Game() {
   const [xNext, setXNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
-  const currentSquares = history[history.length - 1];
+
+  const [currentMove, setCurrentMove] = useState(0);
+  const squares = history[currentMove];
+
+  const handleJump = (i) => {
+    setCurrentMove(i);
+    setXNext(i % 2 === 0);
+  };
+
+  const movesRecord = history.map((move, index) => {
+    let description;
+    if (index > 0) {
+      description = "Go to move #" + index;
+    } else {
+      description = "Go to start";
+    }
+    return (
+      <li key={index}>
+        <button onClick={() => handleJump(index)}>{description}</button>
+      </li>
+    );
+  });
 
   function handlePlay(nextSquares) {
-    setHistory([...history, nextSquares]);
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
     setXNext(!xNext);
   }
 
   return (
     <>
       <div className="flex flex-row gap-20">
-        <Board xNext={xNext} squares={currentSquares} onPlay={handlePlay} />
+        <Board xNext={xNext} squares={squares} onPlay={handlePlay} />
         <div className="history">
-          <ol className="text-gray-500 text-xl font-mono">helo</ol>
+          <ol className="text-gray-600 text-xl font-mono list-decimal">
+            {movesRecord}
+          </ol>
         </div>
       </div>
     </>
